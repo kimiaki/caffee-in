7f76b312d82aba4ba7291c7e888c8c59894867cd
kimiaki <flyaway-to-the-sky@hotmail.co.jp>
2020-02-27 16:04:03 +0900

    update main.py

--- main.py
+++ main.py
@@ -8,31 +8,36 @@ import sys
 app=Flask(__name__)
 
 #環境変数の取得
-YOUR_CHANNEL_ACCESS_TOKEN=os.environ['LINE_CHANNEL_SECRET']
-YOUR_CHANNEL_SECRET=os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+LINE_CHANNEL_ACCESS_TOKEN =os.environ['LINE_CHANNEL_SECRET']
+LINE_CHANNEL_SECRET =os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+line_bot_api=LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler=WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback",methods=["POST"])
 def callback():
+    # get X-Line-Signature header value
     signature=request.headers["X-Line-Signature"]
-
+    
+    # get request body as text
     body=request.get_data(as_text=True)
-    app.logger.info("Request body"+body)
-
+    app.logger.info("Request body: " + body)
+    
+    # handle webhook body
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return "OK"
 
-@handler.add(MessageEvent,message=TextMessage)
+# MessageEvent
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text = "「" + event.message.text + "」っていうコーヒーがあるの？")
+        TextSendMessage(text='「' + event.message.text + '」っていうコーヒーがあるの？')
     )
 
 if __name__=="__main__":
-    port=int(os.getenv("PORT",5000))
+    #port=int(os.getenv("PORT",5000))
+    port=int(os.getenv("PORT"))
     app.run(host="0.0.0.0",port=port)
